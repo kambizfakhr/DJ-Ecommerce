e0d46af2afe3883e8918d77d639a3536f09f04f0
kambiz fakhr <83270531+kambizfakhr@users.noreply.github.com>
2022-04-01 17:06:57 +0430

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,42 @@
+var updateBtns = document.getElementsByClassName('update-cart');
+
+for( var i =0 ; i< updateBtns.length ; i++)
+{
+    updateBtns[i].addEventListener('click', function(){
+        var productID = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('Product ID:', productID , ' Action:',action);
+        console.log('User: ', user);
+
+        if(user==='AnonymousUser'){
+            console.log('Your are not logged in');
+        }else{
+            updateUserOrder(productID ,action)
+        }
+    })
+}
+
+function updateUserOrder(productID ,action){
+    console.log('You are Logged in, data send...');
+    var url = '/update_item/'
+
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken' : csrftoken,
+        },
+        body:JSON.stringify({
+            'productID':productID,
+              'action': action,
+        })
+    })
+    
+    .then((response) => {
+        return response.json()
+    })
+    
+    .then((data) =>{
+        console.log('Data:', data);
+    })
+}
